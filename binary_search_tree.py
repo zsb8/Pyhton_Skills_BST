6f529c5bfc580242829fed025272336e9e5f38ff
zsb8 <songbin.zhang@gmail.com>
2023-01-10 11:34:34 -0500

    first version

---
+++ binary_search_tree.py
@@ -0,0 +1,18 @@
+import collections
+
+Node = collections.namedtuple('Node', ['left', 'right', 'value'])
+n1 = Node(value=1, left=None, right=None)
+n3 = Node(value=3, left=None, right=None)
+n2 = Node(value=2, left=n1, right=n3)
+
+def contains(root, value):
+    if root is None:
+        return False
+    elif root.value == value:
+        return True
+    elif root.value >= value:
+        return contains(root.left, value)
+    else:
+        return contains(root.right, value)
+
+print(contains(n2, 3))
